5aa4967c29517acdc2afbcbac1764bc9afd2cbe0
Surendra Tarai <tarai.surendra@gmail.com>
2023-11-14 20:28:43 +0530

    modified 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get(['/', '/hello'], (req, res) => {
-    res.send('Hello world');
+    res.send('Hello modified 1');
 });
 
 app.listen(3000, ()=> console.log('Listening on port 3000...'));
